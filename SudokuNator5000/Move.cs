9f3248f5879b4c469da883918c42591f7f8bbdd1
Arenzon <arenzonlior@gmail.com>
2026-01-31 16:00:23 +0200

    basic move class

---
+++ SudokuNator5000/Move.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuNator5000
+{
+    internal class Move
+    {
+        private int row, col, old_val, new_value;
+
+        public Move (int row, int col, int old_val, int new_value)
+        {
+            this.row = row;
+            this.col = col;
+            this.old_val = old_val;
+            this.new_value = new_value;
+        }
+
+        public int Row { get { return row; } }
+        public int Col { get { return col; } }
+        public int OldVal { get { return old_val; } }
+        public int NewVal { get { return new_value; } }
+
+        public void Revert(Square[,] squares)
+        {
+            squares[row, col].SolveFor(old_val);
+        }
+
+    }
+}
