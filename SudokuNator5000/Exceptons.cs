f767182f410cba6ff8513db785da552fd75f36f1
Arenzon <arenzonlior@gmail.com>
2026-02-02 23:23:02 +0200

    added exception classes

---
+++ SudokuNator5000/Exceptons.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuNator5000
+{
+    public class InvalidBoardSizesException : Exception
+    {
+        public InvalidBoardSizesException(string message) : base(message) { }
+    }
+
+    public class InvalidInputException : Exception
+    {
+        public InvalidInputException(string message) : base(message) { }
+    }
+
+
+}
